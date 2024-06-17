6a123d1544148abda1a9653c64615440ef4e7140
CristopherMasserini <crismasserini@gmail.com>
2024-06-17 12:39:48 -0400

    Geography plotting

--- Tests/testLocationAnalysis.py
+++ Tests/testLocationAnalysis.py
@@ -5,5 +5,11 @@ import unittest
 
 
 def test_get_countries():
-    data = locationAnalysis.get_countries('Files/winemag-data-small.csv')
+    data = locationAnalysis.get_countries('Files/winemag-data-small.csv',
+                                          'Files/winemag-data-small_grouped.csv')
     assert list(data.loc[:, 'counts'])[0] > 0
+
+
+def test_plot_countries():
+    plotted = locationAnalysis.plot_countries('Files/winemag-data-small_grouped.csv')
+    assert plotted is True
--- locationAnalysis.py
+++ locationAnalysis.py
@@ -1,8 +1,11 @@
 import cleaning
-import geopandas
-import folium
 
-def get_countries(filePath):
+import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+
+def get_countries(filePath: str, saveFilePath: str):
     """
     Gets the countries out of the data set and the counts at each
 
@@ -12,6 +15,35 @@ def get_countries(filePath):
     data = cleaning.clean_columns(data)
 
     data = data.groupby(['country']).size().reset_index(name='counts')
+    data.to_csv(saveFilePath, index=False)
     return data
 
 
+def plot_countries(filePath):
+    """
+    Plots
+
+    :param filePath: file path of grouped countries
+    """
+    countries = pd.read_csv(filePath)
+    gdf = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    try:
+        gdf = gdf.merge(countries, how='outer', left_on='name', right_on='country')
+        # gdf['counts'] = gdf['counts'].fillna(0)
+
+        fig = gdf.plot(column='counts', cmap='Purples', legend=True,
+                       missing_kwds={"color": "black", "edgecolor": "red",
+                                     "hatch": "///", "label": "No Reviews"}
+                       )
+        fig.axis('off')
+        plt.show()
+        return True
+    except Exception:
+        return False
+
+
+if __name__ == '__main__':
+    get_countries('Files/winemag-data-130k-v2.csv',
+                  'Files/winemag-data-130k-v2_grouped.csv')
+    plot_countries('Files/winemag-data-130k-v2_grouped.csv')
