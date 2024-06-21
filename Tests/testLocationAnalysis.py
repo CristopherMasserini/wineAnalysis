0aa6ab6f26a643cdbdb9815933bb414e95c03fbb
CristopherMasserini <crismasserini@gmail.com>
2024-06-21 13:39:25 -0400

    Average test

--- Tests/testLocationAnalysis.py
+++ Tests/testLocationAnalysis.py
@@ -4,10 +4,17 @@ import pandas as pd
 import unittest
 
 
-def test_get_countries():
+def test_get_countries_counts():
     data = locationAnalysis.get_countries('Files/winemag-data-small.csv',
                                           'Files/winemag-data-small_grouped.csv')
     assert list(data.loc[:, 'counts'])[0] > 0
+
+def test_get_countries_averages():
+    data = locationAnalysis.get_countries('Files/winemag-data-small.csv',
+                                          'Files/winemag-data-small_grouped.csv',
+                                          False)
+    assert list(data.loc[:, 'averagePoints'])[0] > 80
+    assert list(data.loc[:, 'averagePoints'])[0] < 100
 
 
 def test_plot_countries():
